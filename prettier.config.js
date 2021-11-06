ea53b60024f1b9cd31962d03c7ef7aaac55a888a
zipang <christophe.Desguez@gmail.com>
2021-11-06 04:29:04 +0100

    Project configuration files

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "none",
+	arrowParens: "always",
+	bracketSpacing: true,
+	printWidth: 90
+};
