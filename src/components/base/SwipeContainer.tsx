e9b973118d3567a060ca2a5ab44181324ec58317
zipang <christophe.Desguez@gmail.com>
2021-11-06 20:51:34 +0100

    Use framer-motion to handle the drag

--- src/components/base/SwipeContainer.tsx
+++ src/components/base/SwipeContainer.tsx
@@ -1,5 +1,5 @@
-import { useSwipeable } from "react-swipeable";
-import { SwipeableCallbacks } from "react-swipeable/dist/types";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useRef, useState } from "react";
 
 /**
  * @typedef {Object} SwipeContainerProps
@@ -9,28 +9,66 @@ import { SwipeableCallbacks } from "react-swipeable/dist/types";
  * @property {Function} [onSwipedUp] Callback when user has swiped up
  * @property {Function} [onSwipedDown] Callback when user has swiped down
  */
-type SwipeContainerProps = Partial<
-	{
-		children: JSX.Element;
-	} & SwipeableCallbacks
->;
+type SwipeContainerProps = Partial<{
+	children: JSX.Element;
+	onSwipedLeft: Function;
+	onSwipedRight: Function;
+	onSwipedUp: Function;
+	onSwipedDown: Function;
+}>;
 
 /**
  * @param {SwipeContainerProps} props
  */
 export const SwipeContainer = ({
 	children,
-	...swipeEventHandlers
+	onSwipedLeft,
+	onSwipedRight,
+	onSwipedDown,
+	onSwipedUp
 }: SwipeContainerProps): JSX.Element => {
-	const handlers = useSwipeable({
-		...swipeEventHandlers,
-		preventDefaultTouchmoveEvent: true,
-		trackMouse: true
-	});
+	const cardElem = useRef(null);
+	const x = useMotionValue(0);
+	const input = [-400, 0, 400];
+	const output = [0, 1, 0];
+	const opacity = useTransform(x, input, output);
+	const [direction, setDirection] = useState<string | null>(null);
+
+	const determineDirection = () => {
+		if (x.getPrevious() < x.get()) {
+			setDirection("right");
+		} else {
+			setDirection("left");
+		}
+	};
+
+	const onDragEnd = () => {
+		if (direction === "left" && onSwipedLeft) {
+			onSwipedLeft();
+		} else if (direction === "right" && onSwipedRight) {
+			onSwipedRight();
+		} else if (direction === "up" && onSwipedUp) {
+			onSwipedUp();
+		} else if (direction === "down" && onSwipedDown) {
+			onSwipedDown();
+		}
+		x.updateAndNotify(0, true);
+	};
 
 	return (
-		<div className="swipe-container" {...handlers}>
+		<motion.div
+			drag="x"
+			dragDirectionLock={true}
+			className="swipe-container"
+			dragConstraints={{ left: 0, right: 0 }}
+			dragElastic={1}
+			ref={cardElem}
+			style={{ x, opacity }}
+			onDrag={determineDirection}
+			onDragEnd={onDragEnd}
+			whileTap={{ scale: 0.9 }}
+		>
 			{children}
-		</div>
+		</motion.div>
 	);
 };
