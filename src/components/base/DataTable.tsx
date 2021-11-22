4a1a316b3d8a5b109dc97c1c4b2df99462a82bba
zipang <christophe.Desguez@gmail.com>
2021-11-22 13:21:21 +0100

    Working on a flexible DataTable with css grid

---
+++ src/components/base/DataTable.tsx
@@ -0,0 +1,61 @@
+import { Box, SimpleGrid } from "@chakra-ui/layout";
+import { getProperty } from "lib/utils/NestedObjects";
+import React from "react";
+
+const getData = (data, accessor) => {
+	if (typeof accessor === "function") {
+		return accessor(data);
+	} else {
+		getProperty(data, accessor);
+	}
+};
+
+/**
+ * @typedef ColumnDefinition
+ * @property {String|Function} header Label to use as column header
+ * @property {String|Function} accessor How to access the displayed value from a row data
+ * @property {String} width Use fr or a fixed dimension
+ */
+export type ColumnDefinition = {
+	header: string | Function;
+	accessor: string | Function;
+	width: string;
+};
+
+/**
+ * @typedef DataTableProperties
+ * @property {Array<ColumnDefinition>} columnsDef
+ * @property {Array<Object>} rows
+ */
+export type DataTableProperties = {
+	columnsDef: ColumnDefinition[];
+	data: Object[];
+};
+/**
+ *
+ * @param {DataTableProperties} props
+ */
+export const DataTable = ({ columnDefs, data }) => {
+	const templateColumns = columnDefs.reduce(
+		(prec: string, def: string) => prec + " " + def,
+		""
+	);
+
+	return (
+		<SimpleGrid templateColumns={templateColumns}>
+			{
+				// Display the Headers
+				columnDefs.map(({ header, width }, i) => (
+					<Box className="header" key={`column-${i}`} width={width}>
+						{header}
+					</Box>
+				))
+			}
+			{columnDefs.map(({ accessor, width }, i) => (
+				<Box className="column" key={`column`} width={width}>
+					{getData(data, accessor)}
+				</Box>
+			))}
+		</SimpleGrid>
+	);
+};
