368f2977065ebfc115a30784282611f209e78aef
zipang <christophe.Desguez@gmail.com>
2021-11-24 10:10:37 +0100

    Add direct support for the Error object

--- src/components/base/ErrorMessage.tsx
+++ src/components/base/ErrorMessage.tsx
@@ -1,18 +1,32 @@
 import { Center, Code } from "@chakra-ui/layout";
+import { Paragraph } from "./Typography";
 
 /**
  * @typedef ErrorMessageProps
- * @property {Error} error The original javascript error catched
+ * @property {string|Error} error An error message or the original javascript error catched
  */
+type ErrorMessageProps = {
+	error: string | Error;
+};
 /**
  * Display an error
  * @param {ErrorMessageProps} props
  */
-export const ErrorMessage = ({ error }) => (
+export const ErrorMessage = ({ error }: ErrorMessageProps) => (
 	<Center>
-		<p>
-			<Code color="red">{error.message}</Code>
-			<pre>{error.stack}</pre>
-		</p>
+		<Paragraph>
+			{error instanceof Error ? (
+				<>
+					<Code color="red" fontWeight="bold">
+						{error.message}
+					</Code>
+					<pre>{error.stack}</pre>
+				</>
+			) : (
+				<Code color="red">{error}</Code>
+			)}
+		</Paragraph>
 	</Center>
 );
+
+export default ErrorMessage;
