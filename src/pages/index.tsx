8b29b042509fbb55dd7008b04c2986339ad5a01a
zipang <christophe.Desguez@gmail.com>
2021-11-24 10:12:07 +0100

    Introducing the Next(JS) Firestore Startup Template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Box, Center, Heading } from "@chakra-ui/layout";
+import { Heading, VStack } from "@chakra-ui/layout";
 import { withContainerLayout } from "src/layouts/ContainerLayout";
 
 const IndexPage = () => (
-	<Box as="section" mt={10}>
-		<Center>
-			<Heading>CHAKRA IS HERE</Heading>
-		</Center>
-	</Box>
+	<VStack bg="white" mt={6} p={4} boxShadow={10} height="50vh">
+		<Heading>
+			<i>Introducing</i> THE NEXT FIRESTORE STARTUP TEMPLATE
+		</Heading>
+	</VStack>
 );
 
 export default withContainerLayout(IndexPage, true);
