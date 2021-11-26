904168974a415976dc324e9ccf1f79186f8105b5
zipang <christophe.Desguez@gmail.com>
2021-11-26 08:36:38 +0100

    More variants : parseParam(), parseArrayParam()

--- src/lib/utils/Parameters.ts
+++ src/lib/utils/Parameters.ts
@@ -6,6 +6,76 @@ import { NextApiRequest } from "next";
 export type QueryParameter = string | string[] | undefined;
 
 /**
+ * Parse a parameter that may have multiple aliases (names variations)
+ *
+ * @param {Object} source The NextJS Api query object
+ * @param {String|Array<String>} names Aliases for the parameter names
+ * @param {Boolean} [required=false] Is it required ? (throws an exception when not present)
+ * @returns {String|undefined}
+ * @throws {TypeError}
+ */
+export const parseParam = (
+	source: NextApiRequest["query"],
+	names: string | string[],
+	required = false
+): string | undefined => {
+	// Passing only one parameter name
+	if (typeof names === "string") {
+		names = [names];
+	}
+
+	// Read inside the source (the half-parsed query)
+	const str: QueryParameter = names.reduce(
+		(found: QueryParameter, name) => (found !== undefined ? found : source[name]),
+		undefined
+	);
+
+	if (typeof str === "string") {
+		return str;
+	} else if (required) {
+		throw new TypeError(`Parameter ${names.join("|")} (string) is required`);
+	}
+	return undefined;
+};
+
+/**
+ * Parse a multi value parameter that may have multiple aliases (names variations)
+ *
+ * @param {Object} source The NextJS Api query object
+ * @param {String|Array<String>} names Aliases for the parameter names
+ * @param {Boolean} [required=false] Is it required ? (throws an exception when not present)
+ * @returns {String|undefined}
+ * @throws {TypeError}
+ */
+export const parseArrayParam = (
+	source: NextApiRequest["query"],
+	names: string | string[],
+	required = false
+): string[] => {
+	// Passing only one parameter name
+	if (typeof names === "string") {
+		names = [names];
+	}
+
+	// Read inside the source (the half-parsed query)
+	const str: QueryParameter = names.reduce(
+		(found: QueryParameter, name) => (found !== undefined ? found : source[name]),
+		undefined
+	);
+
+	if (typeof str === "string") {
+		return str.split(",");
+	} else if (Array.isArray(str)) {
+		return str;
+	} else if (required) {
+		throw new TypeError(`Parameter ${names.join("|")} (string[]) is required`);
+	}
+
+	// Nothing found, return an empty array
+	return [];
+};
+
+/**
  * Parse an integer parameter that may have aliases (names variations)
  *
  * @param {Object} source The NextJS Api query object
